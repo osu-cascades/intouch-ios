50d16fe23490de310f046c4161136d4b666d5fc8
gonzalky7 <gonzalky@oregonstate.edu>
2017-10-31 11:51:49 -0700

    iOS template

---
+++ intouch-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  intouch-iOS
+//
+//  Created by Kyleen Gonzalez on 10/31/17.
+//  Copyright © 2017 Kyleen Gonzalez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
