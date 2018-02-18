9463fcc40cdca8fc79b36db66cf4680bedd90655
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-02-17 20:47:03 -0800

    added basic table row functions

--- intouch-ios/ReceivedNotificationsViewController.swift
+++ intouch-ios/ReceivedNotificationsViewController.swift
@@ -4,6 +4,23 @@ import UIKit
 
 class ReceivedNotificationsViewController: UITableViewController {
     
-    var notifications: Notifications!
+    var allNotifications: AllNotifications!
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return allNotifications.recvNotifications.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .value1, reuseIdentifier: "UITableViewCell")
+        
+        let item = allNotifications.recvNotifications[indexPath.row]
+        
+        cell.textLabel?.text = item.title
+        cell.detailTextLabel?.text = item.message
+        
+        return cell
+        
+    }
     
 }
