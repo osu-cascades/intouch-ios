55aad7a4aaf62b15381e655f32effacecf7af67f
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-04-01 21:41:15 -0700

    added login function to login vc

--- intouch-ios/LoginVC.swift
+++ intouch-ios/LoginVC.swift
@@ -3,5 +3,17 @@ import UIKit
 
 class LoginVC: UIViewController {
     
+    //MARK: actions
+    
+    @IBAction func login(_ sender: Any) {
+        let controllerId = "RecvVC"
+        
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let initViewController: UIViewController = storyboard.instantiateViewController(withIdentifier: controllerId) as UIViewController
+        self.present(initViewController, animated: true, completion: nil)
+        
+    }
+    
+    //MARK: outlets
     @IBOutlet weak var loginBtn: UIButton!
 }
