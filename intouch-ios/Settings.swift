869ec90e2c44f02d8447a00d61d7fe5911cfab31
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-04-05 16:50:18 -0700

    added settings file

---
+++ intouch-ios/Settings.swift
@@ -0,0 +1,25 @@
+
+
+import Foundation
+
+class Settings {
+    
+    struct Keys {
+        static let username = "USERNAME"
+        static let password = "PASSWORD"
+        static let loggedIn = "LOGGED_IN"
+    }
+    
+    class func setUsernameAndPassword(username: String, password: String) {
+        UserDefaults.standard.set(username, forKey: Keys.username)
+        UserDefaults.standard.set(password, forKey: Keys.password)
+        UserDefaults.standard.set("true", forKey: Keys.loggedIn)
+    }
+    
+    class func clearUsernameAndPassword() {
+        UserDefaults.standard.set("", forKey: Keys.username)
+        UserDefaults.standard.set("", forKey: Keys.password)
+        UserDefaults.standard.set("false", forKey: Keys.loggedIn)
+    }
+    
+}
