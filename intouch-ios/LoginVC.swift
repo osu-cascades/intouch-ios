87648da26aeb08e514ddb4fd02eddd4636b28c61
Aaronmb123 <bakeaaro@oregonstate.edu>
2018-04-05 16:46:45 -0700

    added non-working auth method

--- intouch-ios/LoginVC.swift
+++ intouch-ios/LoginVC.swift
@@ -8,6 +8,16 @@ class LoginVC: UIViewController {
     //MARK: actions
     @IBAction func login(_ sender: Any) {
         
+        //send request
+        let username: String = (usernameTf?.text)!
+        let password: String = (passwordTf?.text)!
+        //print(password)
+        if username == "" || password == "" {
+            print("Username and Password must not be blank")
+        }
+        
+        sendAuthRequest(username: username, password: password)
+        
         UserDefaults.standard.set("true", forKey: "LOGGED_IN")
         
         let controllerId = "TabBar"
@@ -40,4 +50,51 @@ class LoginVC: UIViewController {
     @IBOutlet weak var loginBtn: UIButton!
     @IBOutlet weak var usernameTf: UITextField!
     @IBOutlet weak var passwordTf: UITextField!
+    
+    //MARK: posts
+    //private let authUrlStr = "https://abilitree-intouch-staging.herokuapp.com/auth"
+    private let authUrlStr = "https://abilitree.herokuapp.com/auth"
+    
+    func sendAuthRequest(username: String, password: String) {
+        var request = URLRequest(url: URL(string: authUrlStr)!)
+        request.httpMethod = "POST"
+        let postString = "username=\(username)&password=\(password)"
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {
+                print("error: \(String(describing: error))")
+                return
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                print("status code: \(httpStatus.statusCode)")
+                print("response: \(String(describing: response))")
+                DispatchQueue.main.async {
+                    self.responseL.text = "connection error"
+                }
+                // error handle
+                return
+            }
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode == 200 {
+                print("status code: \(httpStatus.statusCode)")
+                let responseString = String(data: data, encoding: .utf8)
+                if responseString == "authenticated: \(username)" {
+                    print("\(username) has been authenticated")
+                    DispatchQueue.main.async {
+                        //self.responseL.text = "User authenticated"
+                        SettingsBundleHelper.setUsernameAndPassword(username: username, password: password)
+                        self.usernameTf.text = ""
+                        self.passwordTf.text = ""
+                        //sleep(3)
+                        self.performSegue(withIdentifier: "showNotifications", sender: nil)
+                    }
+                } else {
+                    print("username and/or password is invalid")
+                    DispatchQueue.main.async {
+                        self.responseL.text = "username and/or password is invalid"
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
 }
