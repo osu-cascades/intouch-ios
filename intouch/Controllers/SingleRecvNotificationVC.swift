9d22cb85b4111ad9372e79b3920b4adc7cc22ffc
Brad Cook <cookbrad@oregonstate.edu>
2018-11-15 12:52:31 -0800

    add framework for reply to sender

--- intouch/Controllers/SingleRecvNotificationVC.swift
+++ intouch/Controllers/SingleRecvNotificationVC.swift
@@ -8,7 +8,29 @@ class SingleRecvNotificationVC: UIViewController {
     @IBOutlet var dateField: UITextField!
     @IBOutlet var fromField: UITextField!
     @IBOutlet var messageView: UITextView!
-    @IBOutlet weak var replyToSender: UIButton!
+    @IBOutlet weak var replyToSenderBtn: UIButton!
+    @IBOutlet weak var replyTextField: UITextField!
+   
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    @IBAction func showReplyTextField(_ sender: Any) {
+        sendBtn.isHidden.toggle()
+        replyTextField.isHidden.toggle()
+        replyTextField.becomeFirstResponder()
+    }
+
+    @IBAction func sendReply(_ sender: Any) {
+//        var request = URLRequest(url: URL(string: pushUrlStr)!)
+//        request.httpMethod = "POST"
+//        
+//        let username: String? = Settings.getUsername()
+//        let password: String? = Settings.getPassword()
+//        //let group = groups[groupPv.selectedRow(inComponent: 0)]
+//        let postString = "username=\(username!)&password=\(password!)&title=\(title!)&body=\(message!)&group=\(to!)"
+//        request.httpBody = postString.data(using: .utf8)
+        sendBtn.isHidden.toggle()
+        replyTextField.isHidden.toggle()
+    }
     
     //MARK: Variables
     var notification: Notification! {
@@ -20,6 +42,8 @@ class SingleRecvNotificationVC: UIViewController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        replyTextField.isHidden = true
+        sendBtn.isHidden = true
         titleField.text = notification.title
         dateField.text = notification.datetime
         fromField.text = notification.from
