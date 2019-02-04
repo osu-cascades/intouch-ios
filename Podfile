ae0d14eb69fc8ffd87212e3ae0b14ddb6e6bc323
Brad Cook <cookbrad@oregonstate.edu>
2019-02-03 16:18:14 -0800

    add PushNotifications to intouch-dev podfile

--- Podfile
+++ Podfile
@@ -27,7 +27,7 @@ end
 target 'intouch-dev' do
   # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
   use_frameworks!
-
+  
   # Pods for intouch-dev
-
+   pod 'PushNotifications'
 end
--- Pods/Target Support Files/Pods-intouch/Pods-intouch-frameworks.sh
+++ Pods/Target Support Files/Pods-intouch/Pods-intouch-frameworks.sh
@@ -143,10 +143,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/PushNotifications/PushNotifications.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PushNotifications-iOS10.3/PushNotifications.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "${BUILT_PRODUCTS_DIR}/PushNotifications/PushNotifications.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/PushNotifications-iOS10.3/PushNotifications.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
